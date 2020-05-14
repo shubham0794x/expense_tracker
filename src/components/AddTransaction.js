7c94da3d70791494a33b6c08f445a445fcba242f
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-05-14 23:51:03 +0530

    Components

---
+++ src/components/AddTransaction.js
@@ -0,0 +1,27 @@
+import React, {useState, useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState';
+
+export const AddTransaction = () => {
+  const [text, setText] = useState('');
+  const [amount, setAmount] = useState(0);
+
+  const { addTransaction } = useContext(GlobalContext);
+
+  const onSubmit = e => {
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransaction(newTransaction);
+  }
+
+  return (
+    <>
+      
+    </>
+  )
+}
