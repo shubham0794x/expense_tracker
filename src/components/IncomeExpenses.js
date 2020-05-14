103a7802e7823f3c8429aa1efe02cf2dec203853
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-05-14 23:55:30 +0530

    Update IncomeExpenses.js

--- src/components/IncomeExpenses.js
+++ src/components/IncomeExpenses.js
@@ -17,6 +17,15 @@ export const IncomeExpenses = () => {
   ).toFixed(2);
 
   return (
-    
+    <div className="inc-exp-container">
+        <div>
+          <h4>Income</h4>
+  <p className="money plus">{income}</p>
+        </div>
+        <div>
+          <h4>Expense</h4>
+  <p className="money minus">{expense}</p>
+        </div>
+      </div>
   )
 }
