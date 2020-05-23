217b484cbcb801f33d0ecb44619dc967113ffb3f
Shubham Bhadani <shubham.bhadani07@gmail.com>
2020-05-23 21:24:16 +0530

    Update Transaction.js

---
+++ src/components/Transaction.js
@@ -0,0 +1,11 @@
+import React, {useContext} from 'react';
+import { GlobalContext } from '../context/GlobalState';
+
+
+
+  return (
+    <li className={transaction.amount < 0 ? 'minus' : 'plus'}>
+      {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span><button onClick={() => deleteTransaction(transaction.id)} className="delete-btn">x</button>
+    </li>
+  )
+}
